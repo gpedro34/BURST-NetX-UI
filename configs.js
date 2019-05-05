4477d2286bfc3abaf427bee401621616fc12a6a7
Gonçalo Pedro <gpedro34@users.noreply.github.com>
2019-05-05 21:19:25 +0100

    Docker base configurations

--- configs.js
+++ configs.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 /* eslint prefer-const: "off" */
 /* eslint no-unused-vars: "off" */
 // CONFIGURABLES
-let system = 'WINDOWS'; // 'WINDOWS' or 'LINUX' -> (DEFAULT: 'LINUX')
-let apiDomain = 'BA'; // 'BA' or 'PT' or 'LOCAL' or domain -> (DEFAULT: 'BA')
-const domainPort = 443;
-const useSSL = true;
+let system = "LINUX"; // 'WINDOWS' or 'LINUX' -> (DEFAULT: 'LINUX')
+let apiDomain = "LOCAL"; // 'BA' or 'PT' or 'LOCAL' or domain -> (DEFAULT: 'BA')
+const domainPort = 80;
+const useSSL = false;
 
 // Note: you will need to get a mapsApiKey for your project.
 // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
-const mapsApiKey = 'AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY';
+const mapsApiKey = "AIzaSyD-9tSrke72PouQMnMX-a7eZSW0jkFMBWY";
