be05847983345e5a994e7b41d8fa4e576356e5b9
Gonçalo Pedro <gpedro34@users.noreply.github.com>
2019-05-06 01:03:13 +0100

    added needed API key support to get complete peers from the /peers route

--- configs.js
+++ configs.js
@@ -6,6 +6,7 @@ let system = "LINUX"; // 'WINDOWS' or 'LINUX' -> (DEFAULT: 'LINUX')
 let apiDomain = "LOCAL"; // 'BA' or 'PT' or 'LOCAL' or domain -> (DEFAULT: 'BA')
 const domainPort = 80;
 const useSSL = false;
+const apiKey = "test1";
 
 // Note: you will need to get a mapsApiKey for your project.
 // See: https://developers.google.com/chart/interactive/docs/basic_load_libs#load-settings
